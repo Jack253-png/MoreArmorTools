abd0c4aab8ff174abb63a9efce4e10a673a28bf9
Jack <guoxiuchen20170402@163.com>
2022-05-02 19:13:32 +0800

    v0.5.0.6 file

--- src/main/java/com/mccreater/morearmortools/screen/DebugConfigScreen.java
+++ src/main/java/com/mccreater/morearmortools/screen/DebugConfigScreen.java
@@ -26,6 +26,7 @@ public class DebugConfigScreen extends Screen {
     boolean debug = writer.nowConfig.debug;
     int fontsize = writer.nowConfig.FontSize;
     int fonttype = writer.nowConfig.FontType;
+    boolean cag = writer.nowConfig.changeUiLookAndFeel;
     public DebugConfigScreen(Screen screen){
         super(new TranslatableText("morearmortools.menu.option.debugtitle"));
         this.screen = screen;
@@ -34,11 +35,12 @@ public class DebugConfigScreen extends Screen {
         this.renderBackground(m);
         drawCenteredText(m, this.textRenderer, this.title, this.width / 2,20,rgb2int.toint("ffffff"));
         drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.warn"), this.width / 2,this.height / 6 + 148,rgb2int.toint("fe2a1f"));
-        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.isdebug"),this.width / 4,starty,rgb2int.toint("ffffff"));
-        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.serverfontsize"),this.width / 4,gety(1,0),rgb2int.toint("ffffff"));
-        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.serverfonttype"), this.width / 4, gety(2,0),rgb2int.toint("ffffff"));
-        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.open.github.project"), this.width / 4,gety(3,0),rgb2int.toint("ffffff"));
-        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.open.github.report"), this.width / 4,gety(4,0),rgb2int.toint("ffffff"));
+        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.isdebug"),this.width / 4,gety(0,0),rgb2int.toint("ffffff"));
+        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.changelook"),this.width / 4,gety(1,0),rgb2int.toint("ffffff"));
+        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.serverfontsize"),this.width / 4,gety(2,0),rgb2int.toint("ffffff"));
+        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.serverfonttype"), this.width / 4, gety(3,0),rgb2int.toint("ffffff"));
+        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.open.github.project"), this.width / 4,gety(4,0),rgb2int.toint("ffffff"));
+        drawCenteredText(m, this.textRenderer, new TranslatableText("morearmortools.menu.option.open.github.report"), this.width / 4,gety(5,0),rgb2int.toint("ffffff"));
         super.render(m,x,y,d);
     }
     public void init(){
@@ -51,6 +53,7 @@ public class DebugConfigScreen extends Screen {
             MinecraftClient.getInstance().setScreen(screen);
             writer.nowConfig.debug = debug;
             writer.nowConfig.FontSize = fontsize;
+            writer.nowConfig.changeUiLookAndFeel = cag;
             writer.writeConfig();
             logger.info("config written");
         }));
@@ -58,24 +61,28 @@ public class DebugConfigScreen extends Screen {
             debug = !debug;
             button.setMessage(getText2(debug));
         }));
-        this.addDrawableChild(new ButtonWidget(rightx,gety(1,height / 2),width,height,getText(fontsize),button -> {
+        this.addDrawableChild(new ButtonWidget(rightx, gety(1, height / 2),width,height,getText2(cag),button -> {
+            cag = !cag;
+            button.setMessage(getText2(cag));
+        }));
+        this.addDrawableChild(new ButtonWidget(rightx,gety(2,height / 2),width,height,getText(fontsize),button -> {
             fontsize += 6;
             if (fontsize > 26){
                 fontsize = 14;
             }
             button.setMessage(getText(fontsize));
         }));
-        this.addDrawableChild(new ButtonWidget(rightx,gety(2,height / 2),width,height,getFontType(fonttype),button -> {
+        this.addDrawableChild(new ButtonWidget(rightx,gety(3,height / 2),width,height,getFontType(fonttype),button -> {
             fonttype += 1;
             if (fonttype > 2){
                 fonttype = 0;
             }
             button.setMessage(getFontType(fonttype));
         }));
-        this.addDrawableChild(new ButtonWidget(rightx,gety(3,height / 2),width,height,new TranslatableText("morearmortools.menu.option.open.withbrowser"),button -> {
+        this.addDrawableChild(new ButtonWidget(rightx,gety(4,height / 2),width,height,new TranslatableText("morearmortools.menu.option.open.withbrowser"),button -> {
             Util.getOperatingSystem().open(github_project_url);
         }));
-        this.addDrawableChild(new ButtonWidget(rightx,gety(4,height / 2),width,height,new TranslatableText("morearmortools.menu.option.open.withbrowser"),button -> {
+        this.addDrawableChild(new ButtonWidget(rightx,gety(5,height / 2),width,height,new TranslatableText("morearmortools.menu.option.open.withbrowser"),button -> {
             Util.getOperatingSystem().open(github_report_url);
         }));
     }
